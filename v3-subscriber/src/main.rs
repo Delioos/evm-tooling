9a45d46d89a32a8430293593f9472d462607027c
Delioos <htz_j@protonmail.com>
2024-07-24 22:42:01 +0200

    feat: get some log mais querty pitié je pleure sur le typage + j suis à 2g

--- v3-subscriber/src/main.rs
+++ v3-subscriber/src/main.rs
@@ -1,42 +1,42 @@
 use std::env;
 use alloy::{
     providers::{Provider, ProviderBuilder, WsConnect},
-    sol
+    primitives::{keccak256, Address},
+    rpc::types::{Filter, Log},
+    sol_types::SolEvent,
+    sol,
 };
 use eyre::Result;
-use colored::Colorize; 
+use colored::Colorize;
 use futures_util::StreamExt;
 
+sol! {
+    #[derive(Debug)]
+    contract IUniswapV2Pair {
+        event Swap(address indexed sender, uint amount0In, uint amount1In, uint amount0Out, uint amount1Out, address indexed to);
+        event Sync(uint112 reserve0, uint112 reserve1);
+    }
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<()> {
     let rpc_url: String = env::var("WS_ALTITUDE_ENDPOINT_NOLECHE")
         .expect("RPC_URL not set");
     let ws = WsConnect::new(&rpc_url);
     let provider = ProviderBuilder::new().on_ws(ws).await?;
     
     println!("connecting to {}", rpc_url.black().on_cyan());
-
-
-    // subscribe to new blocks 
-        let sub = provider.subscribe_blocks().await?;
-
-    // Wait and take the next 4 blocks.
-    let mut stream = sub.into_stream().take(4);
-
-    println!("Awaiting blocks...");
-
-    // Take the stream and print the block number upon receiving a new block.
-    let handle = tokio::spawn(async move {
-        while let Some(block) = stream.next().await {
-            println!(
-                "Latest block number: {}",
-                block.header.number.expect("Failed to get block number")
-            );
-        }
-    });
-
-    handle.await?;
-
+    
+    // Subscribe to Swap events
+    let signature = keccak256("Swap(address,uint256,uint256,uint256,uint256,address)");
+    let sub = provider.subscribe_logs(&Filter::new().event_signature(signature)).await?;
+    
+    println!("Awaiting logs... ");
+    
+    let mut stream = sub.into_stream().take(2);
+    while let Some(log) = stream.next().await {
+        println!("Raw log: {:#?}", log);
+    }
 
     Ok(())
 }
